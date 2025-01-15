00c1116df82510f34c3480b4261d75f7d93834c9
Dean Whitson <dwhitson@e4.co.za>
2025-01-15 14:17:19 +0200

    InitCommit

--- HMAC Client (Framework)/HMACCLient/HMACCLient/HMACDelegatingHandler.cs
+++ HMAC Client (Framework)/HMACCLient/HMACCLient/HMACDelegatingHandler.cs
@@ -13,53 +13,8 @@ namespace HMACClient
 {
     public class HMACDelegatingHandler : DelegatingHandler
     {
-        // First obtained the APP ID and API Key from the server
-        // The APIKey MUST be stored securely in db or in the App.Config
         string APPId = System.Configuration.ConfigurationManager.AppSettings["APPId"];
         string APIKey = System.Configuration.ConfigurationManager.AppSettings["APIKey"];
-        //private string APPId = "f40757a1-9d52-4a34-adbf-509d6369ad15";
-        //private string APIKey = "Jvd/yX1CsNwinSb3fCUXyNhkRg0psNWLjhfR7YXUr+I=";
-        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            HttpResponseMessage response = null;
-            string requestContentBase64String = string.Empty;
-            //Get the Request URI
-            string requestUri = HttpUtility.UrlEncode(request.RequestUri.AbsoluteUri.ToLower());
-            //Get the Request HTTP Method type
-            string requestHttpMethod = request.Method.Method;
-            //Calculate UNIX time
-            DateTime epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
-            TimeSpan timeSpan = DateTime.UtcNow - epochStart;
-            string requestTimeStamp = Convert.ToUInt64(timeSpan.TotalSeconds).ToString();
-            //Create the random nonce for each request
-            string nonce = Guid.NewGuid().ToString("N");
-            //Checking if the request contains body, usually will be null wiht HTTP GET and DELETE
-            if (request.Content != null)
-            {
-                // Hashing the request body, so any change in request body will result a different hash
-                // we will achieve message integrity
-                byte[] content = await request.Content.ReadAsByteArrayAsync();
-                MD5 md5 = MD5.Create();
-                byte[] requestContentHash = md5.ComputeHash(content);
-                requestContentBase64String = Convert.ToBase64String(requestContentHash);
-            }
-            //Creating the raw signature string by combinging
-            //APPId, request Http Method, request Uri, request TimeStamp, nonce, request Content Base64 String
-            string signatureRawData = String.Format("{0}{1}{2}{3}{4}{5}", APPId, requestHttpMethod, requestUri, requestTimeStamp, nonce, requestContentBase64String);
-            //Converting the APIKey into byte array
-            var secretKeyByteArray = Convert.FromBase64String(APIKey);
-            //Converting the signatureRawData into byte array
-            byte[] signature = Encoding.UTF8.GetBytes(signatureRawData);
-            //Generate the hmac signature and set it in the Authorization header
-            using (HMACSHA256 hmac = new HMACSHA256(secretKeyByteArray))
-            {
-                byte[] signatureBytes = hmac.ComputeHash(signature);
-                string requestSignatureBase64String = Convert.ToBase64String(signatureBytes);
-                //Setting the values in the Authorization header using custom scheme (hmacauth)
-                request.Headers.Authorization = new AuthenticationHeaderValue("hmacauth", string.Format("{0}:{1}:{2}:{3}", APPId, requestSignatureBase64String, nonce, requestTimeStamp));
-            }
-            response = await base.SendAsync(request, cancellationToken);
-            return response;
-        }
+
     }
 }
